dfdd4b36ed25c9da517b34acc2ee2d91e4362e0a
Matthew Goh <186605436+matthew-goh@users.noreply.github.com>
2025-08-11 09:18:15 +0100

    ADR-1642 Service entry controller and bounced email page (#33)
    * ADR-1609 Updates to test data and page checks
    
    * ADR-1609 Navigation updates for upcoming changes
    
    * ADR-1609 Correspondence address page content
    
    * ADR-1609 CorrespondenceAddressControllerSpec
    
    * Merge ADR-2151 into ADR-1636 (#29)
    
    * ADR-1635 Display CYA page with summary list after doing necessary checks
    
    * ADR-1635 Update messages; add tests for helpers
    
    * ADR-1635 Create placeholder correspondence address controller and view
    
    * ADR-1635 Tests for CYA onPageLoad
    
    * ADR-1635 CYA onSubmit and placeholder confirmation controller and view
    
    * ADR-1635 Add tests and other edits
    
    * ADR-1635 PR bot comments - version updates
    
    * ADR-1635 Address new PR bot comment
    
    * ADR-1635 Use IllegalStateException and add ticket numbers to TODOs
    
    * ADR-2151 Submit contact preferences - connector and use in CheckYourAnswersController
    
    * ADR-2151 Tests relating to SubmitPreferencesConnector
    
    * ADR-1616 Add correspondence address to user answers
    
    * ADR-1616 Version updates
    
    * ADR-2151 Use submission response in session
    
    * ADR-2151 Formatting
    
    * ADR-2151 Don't log error message for submission failure
    
    * Merge ADR-1609 into ADR-1636 (#30)
    
    * ADR-1609 Updates to test data and page checks
    
    * ADR-1609 Navigation updates for upcoming changes
    
    * ADR-1609 Correspondence address page content
    
    * ADR-1609 CorrespondenceAddressControllerSpec
    
    * ADR-1636 Contact preference updated page controller and view
    
    * ADR-1636 Tests for preference updated page
    
    * ADR-1609 Change navigation back to original design
    
    * ADR-1609 Remove change link from correspondence address row on CYA
    
    * ADR-1609 Version update
    
    * Use service entry controller
    
    * ServiceEntryControllerSpec and updates to ContactPreferenceControllerSpec
    
    * ADR-1642: Add new email error controller and view
    
    * ADR-1642 Tidy up controller and view; update README
    
    * ADR-1642 Correction on README
    
    * ADR-1642 EmailErrorControllerSpec
    
    * ADR-1642 ContactPreferenceController correction - populate form if question has been answered
    
    ---------
    
    Co-authored-by: Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>

---
+++ app/controllers/changePreferences/EmailErrorController.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.changePreferences
+
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import play.api.Logging
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.changePreferences.EmailErrorView
+
+import javax.inject.Inject
+
+class EmailErrorController @Inject() (
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: EmailErrorView
+) extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    request.userAnswers.subscriptionSummary.bouncedEmail match {
+      case Some(true) => Ok(view())
+      case _          =>
+        logger.warn("Bounced email flag is not set. Redirecting to journey recovery.")
+        Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
+    }
+  }
+}
