d332b75766a23b8b7da3ae26d35d41af818751b8
lmugnai <11977337+lmugnai@users.noreply.github.com>
2026-03-04 12:56:01 +0000

    update deps + plugins to address security vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.29.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.32.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
     "org.typelevel"     %% "cats-core"                     % "2.13.0",
-    "com.beachape"      %% "enumeratum"                     % "1.9.0",
-    "com.beachape"      %% "enumeratum-play"                % "1.9.0",
+    "com.beachape"      %% "enumeratum"                     % "1.9.5",
+    "com.beachape"      %% "enumeratum-play"                % "1.9.5",
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0"
